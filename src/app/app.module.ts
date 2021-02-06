584c6c7f443f086be2cc224d1718ea2892bc9bb3
ndc <nik.chinenov@gmail.com>
2021-02-07 00:46:18 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodosListComponent } from './components/todos-list/todos-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodosListComponent
   ],
   imports: [
     BrowserModule
